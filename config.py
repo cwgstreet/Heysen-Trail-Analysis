744d1149a9f2faa91c16f621de276be3867c95ba
Carl W Greenstreet <cwgstreet@gmail.com>
2019-08-06 10:23:39 +0930

    Add global variables module

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+  
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Module name:  config.py
+# Purpose:      global variables
+# 
+# Notes:        use to pass global variables between modules
+#
+# Copyright:    2019, release under GPL3. See LICENSE file for details
+#               Carl W Greenstreet <cwgstreet@gmail.com>
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+tripidx = []	
+heysen_data = []
